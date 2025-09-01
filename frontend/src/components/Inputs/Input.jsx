fbe07f40cb9ce0e80e54c2822338e7c9f59999cd
AlibiriFlottando <alimayweather7@gmail.com>
2025-09-01 18:38:46 +0100

    adding components

---
+++ frontend/src/components/Inputs/Input.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { fARegEye, FaRegEyeSlash } from "react";
+const Input = (value, onChange, placeholder, label, type) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+    return (
+        <div>
+        <label className="text-[13px] text-slate-800">{label}</label>
+
+        <div className="input-box">
+            <input
+            type={type == "password" ? showPassword ? "text" : "password" : type}
+            placeholder={placeholder}
+            className="w-full bg-transparent outline-none"
+            value={value}
+            onChange={(e) => onChange(e)}
+            />
+
+            {type === "password" && (
+            <>
+                {showPassword ? (
+                <fARegEye
+                    size={22}
+                    className="text-primary cursor-pointer"
+                    onClick={() => toggleShowPassword()}
+                />
+                ) : (
+                <FaRegEyeSlash
+                    size={22}
+                    className="text-slate-400 cursor-pointer"
+                    onClick={() => toggleShowPassword()}
+                />
+                )}
+            </>
+            )}
+        </div>
+        </div>
+    );
+};
+
+export default Input;
